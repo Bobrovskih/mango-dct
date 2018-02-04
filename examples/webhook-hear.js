cbb3775ee8ee099e328968c629a839f462cba523
Bobrovskih A <bobrovskih.artur@gmail.com>
2018-02-04 11:47:29 +0300

    реализован метод hear

--- examples/webhook-hear.js
+++ examples/webhook-hear.js
@@ -9,6 +9,5 @@ webhook1.hear({ callType: 1 }, e => console.log('just callType 1', e));
 webhook1.hear({ callType: 1, duration: '10' }, e => console.log('callType:1, duration:10', e));
 
 app.use(webhook1.handler);
-
 app.use((req, res) => res.status(404).send({ error: 'not found' }));
 app.listen(8080);
